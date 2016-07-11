fde555d2b523f2b44576d5c2809bf2c783717a0f
RLopez <romariolopezc@gmail.com>
2016-07-11 12:55:26 -0500

    Terminando de probar el controlador de videos. Se agrega un test sincrono de category repo.

--- test/controllers/video_controller_test.exs
+++ test/controllers/video_controller_test.exs
@@ -1,8 +1,8 @@
 defmodule Rumbl.VideoControllerTest do
   use Rumbl.ConnCase
   alias Rumbl.Video
-  @valid_attrs %{url: "http://youtu,be", title: "vid", description: "a vid"}
-  @invalid_attrs %{title: "invalid"}
+  @valid_attrs %{url: "http://youtu.be", title: "vid", description: "a vid"}
+  @invalid_attrs %{url: nil, title: nil, description: nil}
 
   defp video_count(query), do: Repo.one(from v in query, select: count(v.id))
 
@@ -88,67 +88,53 @@ defmodule Rumbl.VideoControllerTest do
     end
   end
 
-  #
-  # alias Rumbl.Video
-  # @valid_attrs %{description: "some content", title: "some content", url: "some content"}
-  # @invalid_attrs %{}
-  #
-  # test "lists all entries on index", %{conn: conn} do
-  #   conn = get conn, video_path(conn, :index)
-  #   assert html_response(conn, 200) =~ "Listing videos"
-  # end
-  #
-  # test "renders form for new resources", %{conn: conn} do
-  #   conn = get conn, video_path(conn, :new)
-  #   assert html_response(conn, 200) =~ "New video"
-  # end
-  #
-  # test "creates resource and redirects when data is valid", %{conn: conn} do
-  #   conn = post conn, video_path(conn, :create), video: @valid_attrs
-  #   assert redirected_to(conn) == video_path(conn, :index)
-  #   assert Repo.get_by(Video, @valid_attrs)
-  # end
-  #
-  # test "does not create resource and renders errors when data is invalid", %{conn: conn} do
-  #   conn = post conn, video_path(conn, :create), video: @invalid_attrs
-  #   assert html_response(conn, 200) =~ "New video"
-  # end
-  #
-  # test "shows chosen resource", %{conn: conn} do
-  #   video = Repo.insert! %Video{}
-  #   conn = get conn, video_path(conn, :show, video)
-  #   assert html_response(conn, 200) =~ "Show video"
-  # end
-  #
-  # test "renders page not found when id is nonexistent", %{conn: conn} do
-  #   assert_error_sent 404, fn ->
-  #     get conn, video_path(conn, :show, -1)
-  #   end
-  # end
-  #
-  # test "renders form for editing chosen resource", %{conn: conn} do
-  #   video = Repo.insert! %Video{}
-  #   conn = get conn, video_path(conn, :edit, video)
-  #   assert html_response(conn, 200) =~ "Edit video"
-  # end
-  #
-  # test "updates chosen resource and redirects when data is valid", %{conn: conn} do
-  #   video = Repo.insert! %Video{}
-  #   conn = put conn, video_path(conn, :update, video), video: @valid_attrs
-  #   assert redirected_to(conn) == video_path(conn, :show, video)
-  #   assert Repo.get_by(Video, @valid_attrs)
-  # end
-  #
-  # test "does not update chosen resource and renders errors when data is invalid", %{conn: conn} do
-  #   video = Repo.insert! %Video{}
-  #   conn = put conn, video_path(conn, :update, video), video: @invalid_attrs
-  #   assert html_response(conn, 200) =~ "Edit video"
-  # end
-  #
-  # test "deletes chosen resource", %{conn: conn} do
-  #   video = Repo.insert! %Video{}
-  #   conn = delete conn, video_path(conn, :delete, video)
-  #   assert redirected_to(conn) == video_path(conn, :index)
-  #   refute Repo.get(Video, video.id)
-  # end
+  @tag login_as: "max"
+  test "renders form for new resources", %{conn: conn, user: _user} do
+    conn = get conn, video_path(conn, :new)
+    assert html_response(conn, 200) =~ "New video"
+  end
+
+  @tag login_as: "max"
+  test "shows chosen resource", %{conn: conn, user: user} do
+    video = insert_video(user, @valid_attrs)
+    conn = get conn, video_path(conn, :show, video)
+    assert html_response(conn, 200) =~ "Show video"
+  end
+
+  @tag login_as: "max"
+  test "renders page not found when id is nonexistent", %{conn: conn, user: _user} do
+    assert_error_sent :not_found, fn ->
+      get conn, video_path(conn, :show, -1)
+    end
+  end
+
+  @tag login_as: "max"
+  test "renders form for editing chosen resource", %{conn: conn, user: user} do
+    video = insert_video(user, @valid_attrs)
+    conn = get conn, video_path(conn, :edit, video)
+    assert html_response(conn, 200) =~ "Edit video"
+  end
+
+  @tag login_as: "max"
+  test "updates chosen resource and redirects when data is valid", %{conn: conn, user: user} do
+    video = insert_video(user, @valid_attrs)
+    conn = put conn, video_path(conn, :update, video), video: %{url: "dev-mx.com"}
+    assert redirected_to(conn) == video_path(conn, :show, video)
+    assert Repo.get_by!(Video, %{url: "dev-mx.com"})
+  end
+
+  @tag login_as: "max"
+  test "does not update chosen resource and renders errors when data is invalid", %{conn: conn, user: user} do
+    video = insert_video(user, @valid_attrs)
+    conn = put conn, video_path(conn, :update, video), video: @invalid_attrs
+    assert html_response(conn, 200) =~ "check the errors"
+  end
+
+  @tag login_as: "max"
+  test "deletes chosen resource", %{conn: conn, user: user} do
+    video = insert_video(user, @valid_attrs)
+    conn = delete conn, video_path(conn, :delete, video)
+    assert redirected_to(conn) == video_path(conn, :index)
+    refute Repo.get(Video, video.id)
+  end
 end
